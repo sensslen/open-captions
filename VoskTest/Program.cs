2ddd3e2ddeb2df0d10e7cde141eda5dee6264d68
rparker-rv <126202365+rparker-rv@users.noreply.github.com>
2023-12-12 14:19:38 -0800

    Added code to send it to message #0 in Pro, too

--- VoskTest/Program.cs
+++ VoskTest/Program.cs
@@ -1,6 +1,18 @@
-﻿using System.Text.Json;
+﻿using System.Net.Http.Json;
+using System.Text.Json;
 using NAudio.Wave;
 using Vosk;
+
+var host = "http://localhost:32475/";
+var messageId = "0";
+
+var httpClient = new HttpClient();
+
+var messageGetter = httpClient.GetAsync($"{host}v1/message/{messageId}");
+messageGetter.Wait();
+var message = JsonSerializer.Deserialize<Message>(messageGetter.Result.Content.ReadAsStream());
+
+Console.WriteLine(message.message);
 
 var model = new Model("vosk-model-small-en-us-0.15");
 
@@ -20,6 +32,7 @@ waveIn.DataAvailable += (s, a) =>
         var result = JsonSerializer.Deserialize<RecognizerResult>(json);
         // Console.WriteLine(json);
         Console.WriteLine(result?.text);
+        SendToPro(result?.text);
     }
 };
 
@@ -28,8 +41,41 @@ waveIn.StartRecording();
 Console.WriteLine("Press return to stop transcribing");
 Console.ReadLine();
 
+void SendToPro(string? text)
+{
+    message.message = text;
+    var content = JsonContent.Create<Message>(message);
+    var messagePutter = httpClient.PutAsync($"{host}v1/message/{messageId}", content);
+    messagePutter.Wait();
+
+    var emptyTrigger = new List<Token>();
+    var triggerContent = JsonContent.Create(emptyTrigger.ToArray());
+    var trigger = httpClient.PostAsync($"{host}v1/message/{messageId}/trigger", triggerContent);
+    trigger.Wait();
+}
+
 public class RecognizerResult
 {
     public string? text { get; set; }
 };
 
+public class Id
+{
+    public int index { get; set; }
+    public string? name { get; set; }
+    public string? uuid { get; set; }
+};
+
+public class Token
+{
+
+};
+
+public class Message
+{
+    public Id id { get; set; }
+    public string? message { get; set; }
+    public Token[] tokens { get; set; }
+    public Id theme { get; set; }
+    public bool visible_on_network { get; set; }
+};
