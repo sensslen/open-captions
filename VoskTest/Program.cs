1b3e1255769e356439291bf3ee758b817a8fd7f6
Simon Ensslen <sensslen@gmail.com>
2023-12-12 20:45:14 +0100

    Add test app to test vosk

---
+++ VoskTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using NAudio.Wave;
+using Vosk;
+
+
+var model = new Model("vosk-model-small-en-us-0.15");
+
+var capture = new WasapiLoopbackCapture();
+var recognizer = new VoskRecognizer(model, capture.WaveFormat.SampleRate);
+
+recognizer.SetMaxAlternatives(0);
+recognizer.SetWords(true);
+
+capture.DataAvailable += (s, a) =>
+{
+    if (recognizer.AcceptWaveform(a.Buffer, a.BytesRecorded))
+    {
+        Console.WriteLine(recognizer.Result());
+    }
+};
+
