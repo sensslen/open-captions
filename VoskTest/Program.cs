8c9bbd9a230ed0e674564ba23533923b0265037b
rparker-rv <126202365+rparker-rv@users.noreply.github.com>
2023-12-12 12:57:34 -0800

    This seems to actually work with the default audio input

--- VoskTest/Program.cs
+++ VoskTest/Program.cs
@@ -1,16 +1,17 @@
 ﻿using NAudio.Wave;
 using Vosk;
 
-
 var model = new Model("vosk-model-small-en-us-0.15");
 
-var capture = new WasapiLoopbackCapture();
-var recognizer = new VoskRecognizer(model, capture.WaveFormat.SampleRate);
+var waveIn = new WaveInEvent(); // WasapiLoopbackCapture()
+waveIn.WaveFormat = new WaveFormat(16000, 1);
+
+var recognizer = new VoskRecognizer(model, waveIn.WaveFormat.SampleRate);
 
 recognizer.SetMaxAlternatives(0);
 recognizer.SetWords(true);
 
-capture.DataAvailable += (s, a) =>
+waveIn.DataAvailable += (s, a) =>
 {
     if (recognizer.AcceptWaveform(a.Buffer, a.BytesRecorded))
     {
@@ -18,3 +19,8 @@ capture.DataAvailable += (s, a) =>
     }
 };
 
+waveIn.StartRecording();
+while (true)
+{
+    Thread.Sleep(250);
+}
