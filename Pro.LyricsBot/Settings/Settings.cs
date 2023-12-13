77dabbf03c08d0dedd11fc6e702feb19b4a34920
stetson-bender <sbender@renewedvision.com>
2023-12-13 14:26:05 -0500

    PR Point

---
+++ Pro.LyricsBot/Settings/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace Pro.LyricsBot.Settings
+{
+    public static class Settings
+    {
+        public static string? SourceDeviceId { get; set; }
+        public static string? ProPresenterHost { get; set; }
+        public static int ProPresenterPort { get; set; }
+        public static int LineLength { get; set; }
+        public static int LineCount { get; set; }
+    }
+}
