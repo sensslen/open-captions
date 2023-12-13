f4069b507c14b866a10d70a6f5a8f284513c24da
Simon Ensslen <sensslen@gmail.com>
2023-12-13 22:32:33 +0100

    make it work 2

---
+++ Pro.LyricsBot/Services/IVoskModelDescriptor.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Renewed Vision, LLC. All rights reserved.
+
+namespace Pro.LyricsBot.Services
+{
+    public interface IVoskModelDescriptor
+    {
+        string Name { get; }
+        string Id { get; }
+    }
+}
--- Pro.LyricsBot/ViewModels/ISettingsVM.cs
+++ Pro.LyricsBot/ViewModels/ISettingsVM.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Pro.LyricsBot.Services;
 
 namespace Pro.LyricsBot.ViewModels
@@ -7,13 +8,17 @@ namespace Pro.LyricsBot.ViewModels
     public interface ISettingsVM : INotifyPropertyChanged
     {
         public ObservableCollection<IAudioDeviceDescription> Devices { get; }
+        public ObservableCollection<IVoskModelDescriptor> Models { get; }
 
         public IAudioDeviceDescription? SelectedAudioSource { get; set; }
+        public IVoskModelDescriptor? SelectedTranscriptionModel { get; set; }
         string? ProPresenterHost { get; set; }
         int ProPresenterPort { get; set; }
         int LineLength { get; set; }
         int LineCount { get; set; }
         string MessageId { get; set; }
         string TokenName { get; set; }
+        string StartStopLabel { get; }
+        IRelayCommand StartStopCommand { get; }
     }
 }
