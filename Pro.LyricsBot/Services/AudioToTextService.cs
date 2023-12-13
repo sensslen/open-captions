40ca7898d5887f6e1f5bc66f5095532617850468
Simon Ensslen <sensslen@gmail.com>
2023-12-13 15:16:38 +0100

    add interface for speetch recognizion

---
+++ Pro.LyricsBot/Services/AudioToTextService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro.LyricsBot.Services
+{
+
+    internal class AudioToTextService : IAudioToTextService
+    {
+        public IObservable<string> WhenRecognizedTextChanged => throw new NotImplementedException();
+
+        public IObservable<string> WhenRecognitionEnded => throw new NotImplementedException();
+    }
+}
---
+++ Pro.LyricsBot/Services/IAudioToTextService.cs
@@ -0,0 +1,8 @@
+﻿namespace Pro.LyricsBot.Services
+{
+    public interface IAudioToTextService
+    {
+        IObservable<string> WhenRecognizedTextChanged { get; }
+        IObservable<string> WhenRecognitionEnded { get; }
+    }
+}
