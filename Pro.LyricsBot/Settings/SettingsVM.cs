384baae313a5d355df269f4a77a8b8b5b8eccd18
stetson-bender <sbender@renewedvision.com>
2023-12-13 15:53:49 -0500

    Use picker for devices

--- Pro.LyricsBot/Settings/SettingsVM.cs
+++ Pro.LyricsBot/Settings/SettingsVM.cs
@@ -159,7 +159,11 @@ namespace Pro.LyricsBot.Settings
 
         protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
         {
-            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+
             field = value;
             OnPropertyChanged(propertyName);
             return true;
