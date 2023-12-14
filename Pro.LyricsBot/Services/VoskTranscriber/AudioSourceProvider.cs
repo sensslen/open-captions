6605c59fef4ca7692a59e5154acad5a98c7ec55f
Simon Ensslen <sensslen@gmail.com>
2023-12-14 16:51:08 +0100

    Allow for loopback devices

--- Pro.LyricsBot/Services/VoskTranscriber/AudioSourceProvider.cs
+++ Pro.LyricsBot/Services/VoskTranscriber/AudioSourceProvider.cs
@@ -9,11 +9,14 @@ namespace Pro.LyricsBot.Services.VoskTranscriber
 
     public class AudioSourceProvider : IAudioSourceProvider
     {
-        public IList<ISelectionDescriptor> GetAvailable() =>
-            new MMDeviceEnumerator().
-                EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.All).
-            Where(device => device.State == DeviceState.Active).
-            Select(d => (ISelectionDescriptor)new WrappedAudioDevice(d)).ToList();
+        public IList<ISelectionDescriptor> GetAvailable()
+        {
+            var enumerator = new MMDeviceEnumerator();
+            var devices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).
+                Select(d => (ISelectionDescriptor)new WrappedAudioDevice(d));
+            return devices.Concat(enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active).
+                Select(d => (ISelectionDescriptor)new LoopbackAudioDevice(d))).ToList();
+        }
 
         public IWaveIn? Open(ISelectionDescriptor? description)
         {
@@ -25,7 +28,22 @@ namespace Pro.LyricsBot.Services.VoskTranscriber
                 result.StartRecording();
                 return result;
             }
+            else if (description is LoopbackAudioDevice loopback)
+            {
+                var result = new WasapiLoopbackCapture(loopback.Device);
+                result.WaveFormat = new WaveFormat(16000, 1);
+
+                result.StartRecording();
+                return result;
+            }
             return default;
+        }
+
+        private sealed record LoopbackAudioDevice(MMDevice Device) : ISelectionDescriptor
+        {
+            public string Id => $"Loopback {Device.ID}";
+
+            public string Name => $"--Loopback-- {Device.FriendlyName}";
         }
 
         private sealed record WrappedAudioDevice(MMDevice Device) : ISelectionDescriptor
